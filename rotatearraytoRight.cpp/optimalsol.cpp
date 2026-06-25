4a46f0d169663d9ce570dc84497bf0178761c19c
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-06-25 08:51:02 +0530

    what? like its hard!

---
+++ rotatearraytoRight.cpp/optimalsol.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+void Reverse(vector<int>& nums, int start, int end){
+    while(start <= end){
+    int temp;
+    temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
+    }
+}
+    void rotate(vector<int>& nums, int k) {
+      int n = nums.size();
+      Reverse(nums, 0, n-k-1);
+      Reverse(nums, n-k, n-1);
+      Reverse(nums, 0, n-1);
+    }
+};//in this leetcode problem we have not used stl library for reverse
