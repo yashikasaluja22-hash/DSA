3ffbbfd3e7be6aba0e3a94d24f1d923bed08d687
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-07-07 21:21:41 +0530

    commentary

--- longestsubarrayofsumk/bettersol.cpp
+++ longestsubarrayofsumk/bettersol.cpp
@@ -13,8 +13,16 @@ for(int i = 0; i < a.size(); i++){
     if(presummap.find(rem) != presummap.end()){
         int len = i - presummap[rem];
         maxlen = max(maxlen, len);
-    } 
+    }
+    if(presummap.find(sum) == presummap.end()){
     presummap[sum] = i;
+    }
 }
 return maxlen;
-}//what a problem solution dude
+}
+/*so 17 code line means if there is an array --> 2 0 0 3
+then in above array till i = 2, sum will be same
+so to update hashmap and store it with 2->0, 2->1, 2->2 key-vales
+otherwise it will be like 2->0 then it will updated with different index no. : 2->1
+and then like this 2->2
+so otherwise value of key will changed not new key will not be added/made if sum is same, if u dont write line 17*/
