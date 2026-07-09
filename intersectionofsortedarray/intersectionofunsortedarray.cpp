6f558fd6010ac4e2a3fc6fa815110b1339c8b027
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-07-10 01:17:18 +0530

    haadi mera baddi

--- intersectionofsortedarray/intersectionofunsortedarray.cpp
+++ intersectionofsortedarray/intersectionofunsortedarray.cpp
@@ -1,5 +1,6 @@
 //Given two integer arrays nums1 and nums2, return an array of their intersection.
 //Each element in the result must be unique and you may return the result in any order.
+//this is a leetcode ques
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
